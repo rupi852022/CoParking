41870177b9f4333e20ac4489dbd03328add1869f
idan44441 <95760294+idan44441@users.noreply.github.com>
2022-03-11 09:28:04 +0200

    Start Project

---
+++ Models/DAL/DataServices.cs
@@ -0,0 +1,148 @@
+﻿using ParkingProject.Models;
+using System;
+using System.Data;
+using System.Data.SqlClient;
+
+
+namespace Class_demo.Models.DAL
+{
+    public class DataServices
+    {
+        public class DataServices
+        {
+
+            public int InsertUser(User U)
+            {
+                SqlConnection con = null;
+                try
+                {
+                    // C - Connect
+                    con = Connect("webOsDB");
+
+                    // C - Create Command
+                    SqlCommand command = CreateInsertUser(U, con);
+
+                    // E - Execute
+                    int affected = command.ExecuteNonQuery();
+
+                    return affected;
+
+                }
+                catch (Exception ex)
+                {
+                    // write to log file
+                    throw new Exception("Failed in Insert of User", ex);
+                }
+
+                finally
+                {
+                    // Close Connection
+                    con.Close();
+                }
+            }
+
+
+            SqlCommand CreateInsertUser(User U, SqlConnection con)
+            {
+
+                string insertStr = "INSERT INTO [CoParkingUsers_2022] ([email], [password], [fName], [lName], [phoneNumber], [gender]) VALUES('" + U.Email + "', '" + U.Password + "', '" + U.FName + "', '" + U.LName + "', '" + U.PhoneNumber + "', '" + U.Gender + "')";
+                SqlCommand command = new SqlCommand(insertStr, con);
+                // TBC - Type and Timeout
+                command.CommandType = System.Data.CommandType.Text;
+                command.CommandTimeout = 30;
+                return command;
+
+            }
+
+
+            //Read
+            public User ReadUser(string email)
+            {
+                SqlConnection con = null;
+                SqlDataReader dr = null;
+
+                try
+                {
+                    // Connect
+                    con = Connect("webOsDB");
+
+                    // Create the select command
+                    SqlCommand selectCommand = creatSelectUserCommand(con, email, password);
+
+                    // Create the reader
+                    dr = selectCommand.ExecuteReader(CommandBehavior.CloseConnection);
+
+                    // Read the records
+                    // Execute the command
+                    //int id = Convert.ToInt32(insertCommand.ExecuteScalar());
+
+                    if (dr == null || !dr.Read())
+                    {
+                        return null;
+                    }
+
+                    int id = Convert.ToInt32(dr["id"]);
+                    string email = (string)dr["email"];
+                    string password = (string)dr["password"];
+                    string fName = (string)dr["lName"];
+                    string lName = (string)dr["fName"];
+                    string phoneNumber = (string)dr["phoneNumber"];
+                    string gender = (string)dr["gender"];
+
+
+
+                    User user = new User();
+
+                    if (dr.Read())
+                    {
+                        return null;
+                    }
+
+                    return user;
+                }
+                catch (Exception ex)
+                {
+                    // write the error to log
+                    throw new Exception("failed in Log In", ex);
+                }
+                finally
+                {
+                    if (dr != null)
+                    {
+                        dr.Close();
+                    }
+
+                    // Close the connection
+                    if (con != null)
+                        con.Close();
+                }
+            }
+
+            private SqlCommand creatSelectUserCommand(SqlConnection con, string email)
+            {
+                string commandStr = "SELECT * FROM CoParkingUsers_2022 WHERE email=@email";
+                SqlCommand cmd = createCommand(con, commandStr);
+                cmd.Parameters.AddWithValue("@email", email);
+
+                return cmd;
+            }
+
+
+            SqlCommand createCommand(SqlConnection con, string CommandSTR)
+            {
+
+                SqlCommand cmd = new SqlCommand();  // create the command object
+                cmd.Connection = con;               // assign the connection to the command object
+                cmd.CommandText = CommandSTR;       // can be Select, Insert, Update, Delete
+                cmd.CommandType = System.Data.CommandType.Text;
+                cmd.CommandTimeout = 5; // seconds
+
+                return cmd;
+            }
+        }
+
+
+
+    }
+}
+
---
+++ Models/User.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ParkingProject.Models
+{
+    public class User
+    {
+        public class User
+        {
+
+            private int id;
+            private string fName;
+            private string lName;
+            private string email;
+            private string password;
+            private string gender;
+            private string phoneNumber;
+
+            public User() { }
+
+
+            public User(string fName, string lName, string email, string password, string gender, string phoneNumber, int id)
+            {
+                this.fName = fName;
+                this.lName = lName;
+                this.email = email;
+                this.password = password;
+                this.gender = gender;
+                this.phoneNumber = phoneNumber;
+                this.id = id;
+            }
+
+            public string FName { get => fName; set => fName = value; }
+            public string LName { get => lName; set => lName = value; }
+            public string Email { get => email; set => email = value; }
+            public string Password { get => password; set => password = value; }
+            public string Gender { get => gender; set => gender = value; }
+            public string PhoneNumber { get => phoneNumber; set => phoneNumber = value; }
+            public int Id { get => id; set => id = value; }
+
+            public int Insert()
+            {
+                DataServices ds = new DataServices();
+                User user = ds.ReadUser(this.email);
+
+                if (user != null)
+                {
+                    return -1; // USER already exist
+                }
+
+                int status = ds.InsertUser(this);
+                return status;
+            }
+
+            public static User readUser(string email, string password)
+            {
+                DataServices ds = new DataServices();
+                User user = ds.ReadUser(email);
+
+                if (user == null || user.password != password)
+                {
+                    return null; // user not exist or password is wrong
+            }
+
+                return user;
+            }
+
+        }
+    }
+}
+
