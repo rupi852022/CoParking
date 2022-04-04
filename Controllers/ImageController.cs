b42309e2b17865219928e2eaa84ccb78cde02a98
idan44441 <95760294+idan44441@users.noreply.github.com>
2022-04-04 19:07:58 +0300

    /

--- Controllers/ImageController.cs
+++ Controllers/ImageController.cs
@@ -1,9 +1,21 @@
-﻿using System;
+﻿//using System;
+//using System.Collections.Generic;
+using System.IO;
+//using System.Linq;
+//using System.Net;
+//using System.Net.Http;
+using System.Web;
+using System.Web.Hosting;
+//using System.Web.Http;
+//using ReactFinalProject.Models;
+using ParkingProject.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+
 
 namespace ParkingProject.Controllers
 {
@@ -22,8 +34,31 @@ namespace ParkingProject.Controllers
         }
 
         // POST api/<controller>
-        public void Post([FromBody] string value)
+
+        public HttpResponseMessage Post(string filePathDes)
         {
+
+            HttpResponseMessage result = null;
+            var httpRequest = HttpContext.Current.Request;
+            if (httpRequest.Files.Count > 0)
+            {
+                var docfiles = new List<string>();
+                foreach (string file in httpRequest.Files)
+                {
+                    var postedFile = httpRequest.Files[file];
+                    var filePath = HttpContext.Current.Server.MapPath("~/Images/" + filePathDes + "/" + postedFile.FileName);
+                    postedFile.SaveAs(filePath);
+                    //docfiles.Add(filePath);
+                    docfiles.Add("../Images/" + filePathDes + "/" + postedFile.FileName);
+
+                }
+                result = Request.CreateResponse(HttpStatusCode.Created, docfiles);
+            }
+            else
+            {
+                result = Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
+            return result;
         }
 
         // PUT api/<controller>/5
